c1ace241586340b39836eae65f06ab309c2c85ad
Jordi Pradel <jordi.pradel@agilogy.com>
2022-05-28 00:03:17 +0200

    Clean unused content

--- publish-staging-drafts.sh
+++ publish-staging-drafts.sh
@@ -1,2 +1,3 @@
-bundle exec jekyll build --drafts
+bundle exec jekyll build --drafts 
 aws s3 sync ./_site s3://agilogy-blog-staging/ 
+open http://agilogy-blog-staging.s3-website-eu-west-1.amazonaws.com/
